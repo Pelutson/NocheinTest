bba2e110c48521e56cd5483347eb97dd4da72fec
JanMiehe <miehejan8@gmail.com>
2025-02-03 13:47:35 -0600

    1

--- main.cpp
+++ main.cpp
@@ -2,74 +2,65 @@
 #include <string>
 using namespace std;
 
-// Class Definition
-class Place {
-public:
-    // Constructor
-    Place() {
-        cout << "You're in the Cell" << endl;
-    }
 
-    // Member functions for different places
-    void Cell() {
-        cout << "You're in the Cell" << endl;
-    }
+void Cell() {
+    cout << "You're in the Cell" << endl;
+}
 
-    void Hub() {
-        cout << "You're in the Hub" << endl;
-    }
+void Hub() {
+    cout << "You're in the Hub" << endl;
+}
 
-    void Shower() {
-        cout << "You're in the Shower" << endl;
-    }
+void Shower() {
+    cout << "You're in the Shower" << endl;
+}
 
-    void Cafeteria() {
-        cout << "You're in the Cafeteria" << endl;
-    }
+void Cafeteria() {
+    cout << "You're in the Cafeteria" << endl;
+}
 
-    void Library() {
-        cout << "You're in the Library" << endl;
-    }
+void Library() {
+    cout << "You're in the Library" << endl;
+}
 
-    void Courtyard() {
-        cout << "You're in the Courtyard" << endl;
-    }
+void Courtyard() {
+    cout << "You're in the Courtyard" << endl;
+}
 
-    void Basketball() {
-        cout << "You're in the Basketball" << endl;
-    }
+void Basketball() {
+    cout << "You're in the Basketball" << endl;
+}
 
-    void Benchpress() {
-        cout << "You're in the Benchpress" << endl;
-    }
-};
+void Benchpress() {
+    cout << "You're in the Benchpress" << endl;
+}
+
 
 int main() {
-    string place;
-    // Create an object of the class Place
-    Place myplace;  // Calls the constructor and prints "You're in the Cell"
 
+    string place;
+    
     cout << "Where do you want to go?" << endl;
     cout << "[Cell/Hub/Shower/Cafeteria/Library/Courtyard/Basketball/Benchpress]: ";
     cin >> place;
 
     // Check user input and call the corresponding member function
     if (place == "Cell") {
-        myplace.Cell();
+        Cell();
     } else if (place == "Hub") {
-        myplace.Hub();
+        Hub();
     } else if (place == "Shower") {
-        myplace.Shower();
+        Shower();
     } else if (place == "Cafeteria") {
-        myplace.Cafeteria();
+        Cafeteria();
     } else if (place == "Library") {
-        myplace.Library();
+        Library();
     } else if (place == "Courtyard") {
-        myplace.Courtyard();
+        Courtyard();
     } else if (place == "Basketball") {
-        myplace.Basketball();
+        Basketball();
     } else if (place == "Benchpress") {
-        myplace.Benchpress();
+        Benchpress();
     } else {
         cout << "Invalid place entered." << endl;
     }
