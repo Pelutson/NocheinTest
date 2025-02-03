dbfbe3792270631142e1973c089f40f52b533c13
Pelutson <marc.miehe@icloud.com>
2025-02-03 20:49:43 +0100

    Meine Idee

--- main.cpp
+++ main.cpp
@@ -1,78 +1,47 @@
-include <iostream>
-#include <string>
+#include <iostream>
 using namespace std;
 
-// Class Definition
-class Place {
-public:
-    // Constructor
-    Place() {
-        cout << "You're in the ";
-    }
+// Funktion 1: Gibt eine entsprechende Nachricht aus.
+void funktion1() {
+    cout << "Funktion 1 wurde aufgerufen." << endl;
+}
 
-    // Member functions for different places
-    void Cell() {
-        cout << "Cell" << endl;
-    }
+// Funktion 2: Gibt eine andere Nachricht aus.
+void funktion2() {
+    cout << "Funktion 2 wurde aufgerufen." << endl;
+}
 
-    void Hub() {
-        cout << "Hub" << endl;
-    }
-
-    void Shower() {
-        cout << "Shower" << endl;
-    }
-
-    void Cafeteria() {
-        cout << "Cafeteria" << endl;
-    }
-
-    void Library() {
-        cout << "Library" << endl;
-    }
-
-    void Courtyard() {
-        cout << "Courtyard" << endl;
-    }
-
-    void Basketball() {
-        cout << "Basketball" << endl;
-    }
-
-    void Benchpress() {
-        cout << "Benchpress" << endl;
-    }
-};
+// Funktion 3: Gibt eine weitere Nachricht aus.
+void funktion3() {
+    cout << "Funktion 3 wurde aufgerufen." << endl;
+}
 
 int main() {
-    string place;
-    // Create an object of the class Place
-    Place myplace;  // Calls the constructor and prints "You're in the "
-
-    cout << "Where do you want to go?" << endl;
-    cout << "[Cell/Hub/Shower/Cafeteria/Library/Courtyard/Basketball/Benchpress]: ";
-    cin >> place;
-
-    // Check user input and call the corresponding member function
-    if (place == "Cell") {
-        myplace.Cell();
-    } else if (place == "Hub") {
-        myplace.Hub();
-    } else if (place == "Shower") {
-        myplace.Shower();
-    } else if (place == "Cafeteria") {
-        myplace.Cafeteria();
-    } else if (place == "Library") {
-        myplace.Library();
-    } else if (place == "Courtyard") {
-        myplace.Courtyard();
-    } else if (place == "Basketball") {
-        myplace.Basketball();
-    } else if (place == "Benchpress") {
-        myplace.Benchpress();
-    } else {
-        cout << "Invalid place entered." << endl;
+    int wahl;
+    
+    // Benutzer wird aufgefordert, eine Auswahl zu treffen.
+    cout << "Wähle eine Funktion aus:" << endl;
+    cout << "1 - Aufruf von Funktion 1" << endl;
+    cout << "2 - Aufruf von Funktion 2" << endl;
+    cout << "3 - Aufruf von Funktion 3" << endl;
+    cout << "Deine Wahl: ";
+    cin >> wahl;
+    
+    // Je nach Eingabe wird die entsprechende Funktion aufgerufen.
+    switch (wahl) {
+        case 1:
+            funktion1();
+            break;
+        case 2:
+            funktion2();
+            break;
+        case 3:
+            funktion3();
+            break;
+        default:
+            cout << "Ungültige Auswahl. Bitte wähle 1, 2 oder 3." << endl;
+            break;
     }
-
+    
     return 0;
 }
